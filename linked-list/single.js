599b48a3dfdc2c7178c97150f452fe5fdb578db2
Caitlin Trussell <caitlintrussell@gmail.com>
2018-06-04 17:08:54 -0500

    setup testing

---
+++ linked-list/single.js
@@ -0,0 +1,15 @@
+
+/**** Linked List Class ****/
+
+class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+}
+
+class LinkedListNode {
+  constructor (value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
