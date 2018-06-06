60f974aae1ffb31a6f0965833313c3000ee822b8
Caitlin Trussell <caitlintrussell@gmail.com>
2018-06-06 18:03:02 -0500

    added addToHead function and tests are passing for it

--- linked-list/single.js
+++ linked-list/single.js
@@ -1,15 +1,23 @@
 
 /**** Linked List Class ****/
 
-export class LinkedList {
-  constructor () {
-    this.head = null;
-  }
-}
-
 export class LinkedListNode {
   constructor (value, next = null) {
     this.value = value;
     this.next = next;
+  }
+}
+
+export class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+  addToHead (val) {
+    let LLN = new LinkedListNode(val);
+    if (this.head === null) this.head = LLN;
+    else {
+      LLN.next = this.head;
+      this.head = LLN;
+    }
   }
 }
